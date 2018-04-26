a4c10810f51b0eed66dafea8bfb48728a58441ac
initstring <26131150+initstring@users.noreply.github.com>
2018-04-26 18:22:31 +1000

    updated cleaning script

--- clean.sh
+++ clean.sh
@@ -5,13 +5,13 @@ OUTFILE=$2
 # It might be worth running this script twice. :)
 
 cat $INFILE \
+    | grep -E '[a-z]{2}.* | [a-z]{2}'                            `# must have at least one space and two seq letters` \
     | sed -r "s/([a-zA-Z]* [a-zA-Z]* [a-zA-Z]*)([,.] )/\1\\n/g"  `# Split up longer lines containing , or .` \
-    | sed -e "s/^[ \t]*//;s/[ \t]*$//"                           `# strip whitespace from beginning and end` \
-          -e "s/\(.*\)/\L\1/"                                    `# switch to lowercase` \
+    | sed -e "s/\(.*\)/\L\1/"                                    `# switch to lowercase` \
           -e "s/[-_]/ /g"                                        `# change hyphens & underscores to spaces` \
           -e "s/ \+/ /g"                                         `# combine multiple spaces into one` \
-          -e "s~[^[:alnum:] ]\+~~g"                              `# keep only alphanumeric characters and spaces` \
-    | grep ' '                                                   `# look for at least one space` \
+          -e "s/[^[:punct:]a-z0-9 ]//g"                          `# keep only letters numbers and spaces` \
+          -e "s/^[ \t]*//;s/[ \t]*$//"                           `# strip whitespace from beginning and end` \
     | awk 'length($0)<41 && length($0)>4'                        `# enforce length` \
     | sort | uniq                                                `# sort and remove duplicates` \
 >> ./$OUTFILE
